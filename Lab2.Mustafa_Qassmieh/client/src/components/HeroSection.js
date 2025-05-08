70b11407385eb616b436beb2eca44152f62902f9
Mustafa.Qassmieh01 <mustafqasmieh@gmail.com>
2025-05-08 19:32:05 +0200

    N/A

--- Lab2.Mustafa_Qassmieh/client/src/components/HeroSection.js
+++ Lab2.Mustafa_Qassmieh/client/src/components/HeroSection.js
@@ -1,8 +1,11 @@
 function HeroSection() {
   return (
     <div className="hero">
-      <h1>Welcome to Our Website</h1>
-      <p>Your journey starts here.</p>
+      <h1>Company is Our name</h1>
+      <p>We may not be the smartest</p>
+      <p>we are defenitly not the best</p>
+      <p>and there is no way we are the fastest</p>
+      <p>but we are here</p> 
     </div>
   );
 }
